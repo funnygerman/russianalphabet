13099d0f23a3c353c412644ba99a479de269d2f2
Tim A <other@ametov.net>
2020-06-02 23:40:30 +0200

    Refactoring

--- lib/widget/letter_board.dart
+++ lib/widget/letter_board.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:russianalphabet/consts/letters.dart';
+import 'package:russianalphabet/i18n/app_localizations.dart';
+import 'package:russianalphabet/model/letter.dart';
 
 class LetterBoard extends StatelessWidget {
   final int boardWidth;
@@ -14,42 +16,67 @@ class LetterBoard extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: _addBoardRows(),
+        children: _addBoardRows(context),
       ),
     );
   }
 
-  List<Widget> _addBoardRows() {
+  List<Widget> _addBoardRows(BuildContext context) {
     var rows = <Widget>[];
     for (var i = 0; i < boardHeight; i++) {
       rows.add(Expanded(
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: _addRowElements(i),
+          children: _addRowElements(i, context),
         ),
       ));
     }
     return rows;
   }
 
-  List<Widget> _addRowElements(int rowNumber) {
+  List<Widget> _addRowElements(int rowNumber, BuildContext context) {
     var elements = <Widget>[];
     for (var i = 0; i < boardWidth; i++) {
       elements.add(
         Expanded(
-          child: Container(
-            margin: EdgeInsets.all(4.0),
-            color: Colors.blue,
-            child: Center(
-              child: Text(
-                LETTERS[rowNumber * boardWidth + i].letter,
-              ),
-            ),
+          child: LetterCard(
+            letter: LETTERS[rowNumber * boardWidth + i],
+            showCardText: (letter) {
+              if (letter != EMPTY_LETTER) {
+                return AppLocalizations.of(context)
+                    .string(letter.exampleStrKey);
+              } else {
+                return '';
+              }
+            },
           ),
         ),
       );
     }
     return elements;
+  }
+}
+
+class LetterCard extends StatelessWidget {
+  final Letter letter;
+  final String Function(Letter) showCardText;
+
+  LetterCard({this.letter, this.showCardText});
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () => print('The letter with key ${letter.key} was pressed'),
+      child: Container(
+        margin: EdgeInsets.all(4.0),
+        color: Colors.blue,
+        child: Center(
+          child: Text(
+            showCardText(letter),
+          ),
+        ),
+      ),
+    );
   }
 }
